e8cdbef6f270f64a11212ed4b4676d0d9b0154eb
sankalpjumale <sankalpjumale@gmail.com>
2026-01-23 23:00:50 +0530

    implement send message API

---
+++ app/api/send-message/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { Message } from "@/models/User";
+
+export async function POST(request: Request) {
+    await dbConnect()
+
+    const {username, content} = await request.json()
+
+    try {
+        const user = await UserModel.findOne({username})
+
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: 'User not found'
+                }, {status: 404}
+            )
+        }
+
+        //is user appecting messages
+        if (!user.isAcceptingMessage) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User is not accepting messages"
+                }, {status: 403}
+            )
+        }
+
+        const newMessage = {content, createdAt: new Date()}
+        user.messages.push(newMessage as Message)
+        await user.save()
+
+        return Response.json(
+            {
+                success: true,
+                message: 'Message sent successfully' 
+            }, {status: 200}
+        )
+    } catch (error) {
+        return Response.json(
+            {
+                success: false,
+                message: 'Error in sending message'
+            }, {status: 500}
+        )
+    }
+}
